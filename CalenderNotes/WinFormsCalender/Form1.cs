ed7868fb2ad6146c6c3163ef3d9b6a200afa93ea
AkhilAjith2 <104864079+AkhilAjith2@users.noreply.github.com>
2023-11-15 20:12:39 +0100

    Add files via upload

---
+++ CalenderNotes/WinFormsCalender/Form1.cs
@@ -0,0 +1,57 @@
+namespace WinFormsCalender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string date = monthCalendar.SelectionStart.ToShortDateString();
+            string note = noteTextBox.Text;
+            ListViewItem newItem = new ListViewItem(new[] { date, note });
+            calendarListView.Items.Add(newItem);
+            noteTextBox.Text = "";
+            notifyIcon.ShowBalloonTip(1000, "Item added", "Added new event", ToolTipIcon.Info);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in calendarListView.SelectedItems)
+            {
+                calendarListView.Items.Remove(item);
+            }
+        }
+
+        private void calendarListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            removeButton.Enabled = calendarListView.SelectedItems.Count > 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIconContextMenu.Items.Add("Exit", null, ExitContextMenu_Click);
+        }
+        private void ExitContextMenu_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIconContextMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
